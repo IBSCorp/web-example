5740ba77a95190616fe247ecee9ce900d2b2a5ea
ndildin <ndildin@ibs.ru>
2023-01-24 20:13:27 +0300

    Переход на 3.3.0

---
+++ src/test/java/org/example/elements/ValidationInput.java
@@ -0,0 +1,23 @@
+package org.example.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import ru.ibsqa.qualit.elements.web.TextInput;
+
+import java.util.List;
+
+/**
+ * TODO
+ */
+public class ValidationInput extends TextInput {
+
+    @Override
+    public String getErrorMsg() {
+        List<WebElement> elements = findElements(By.xpath("../small"));
+        if (elements.size()>0) {
+            return elements.get(0).getText();
+        }
+        return null;
+    }
+
+}
